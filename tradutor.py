baeb9500d5c2c051e428a5d3a1984b16bc12c188
Erick <141505095+rick-oss@users.noreply.github.com>
2023-12-14 18:33:20 -0300

    feat: Adiciona flask_caching para melhorar o desempenho com cache

--- tradutor.py
+++ tradutor.py
@@ -1,7 +1,9 @@
 from deep_translator import GoogleTranslator
 from flask import Flask, render_template, request
+from flask_caching import Cache
 
 app = Flask(__name__)
+cache = Cache(app, config={"CACHE_TYPE": "simple"})
 
 
 def tradutor(idioma_padrao, idioma_traduzido, texto):
@@ -18,9 +20,18 @@ def index():
         idioma_traduzido = request.form.get("idioma_traduzido")
         texto_a_traduzir = request.form.get("texto_a_traduzir")
 
-        texto = tradutor(idioma_padrao, idioma_traduzido, texto_a_traduzir)
+        # Gera uma chave única para a combinaçao de idiomas e texto
+        chave_cache = f"{idioma_padrao}_{idioma_traduzido}_{texto_a_traduzir}"
+        # Tenta obter o resultado do cache:
+        texto = cache.get(chave_cache)
 
-        return render_template("index.html", texto=texto )
+        if texto is None:
+            # Se não estiver no cache, realiza a traduçao:
+            texto = tradutor(idioma_padrao, idioma_traduzido, texto_a_traduzir)
+            # Armazena o resultado no cache por um tempo definido(5min, nesse caso):
+            cache.set(chave_cache, texto, timeout=300)
+
+        return render_template("index.html", texto=texto)
 
     return render_template("index.html")
 
